c63889f4914fa6dfef11803dacd4f9842025edcd
Adam Hammes <ahammes@cortexmedia.ca>
2019-08-25 13:46:23 -0400

    Correctly remove listener in useKeyboardShortcut cleanup

--- site/src/utils/useKeyboardShortcut.js
+++ site/src/utils/useKeyboardShortcut.js
@@ -2,11 +2,12 @@ import { useEffect } from "react";
 
 const useKeyboardShortcut = (key, action) => {
   useEffect(() => {
-    const listener = window.addEventListener("keyup", event => {
+    const listener = event => {
       if (event.keyCode === key) {
         action();
       }
-    });
+    };
+    window.addEventListener("keyup", listener);
 
     return () => window.removeEventListener("keyup", listener);
   }, [key, action]);
